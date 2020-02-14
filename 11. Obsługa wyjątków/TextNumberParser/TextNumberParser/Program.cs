50f22aa4b6420557d4021ec732c87bd41ae72d48
Paweł Palacz <ppalacz@edu.cdv.pl>
2020-02-14 13:27:35 +0100

    Added chapter 11. code

---
+++ 11. Obsługa wyjątków/TextNumberParser/TextNumberParser/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TextNumberParser
+{
+    public sealed class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                // ..
+                throw new InvalidOperationException(
+                    "Dowolny wyjątek");
+                // ..
+            }
+            catch (Win32Exception exception)
+                when (exception.ErrorCode == 42)
+            {
+                // Obsługa wyjątku typu Win32Exception
+                // pole error code ma wartość 42
+            }
+            catch (NullReferenceException exception)
+            {
+                // Obsługa wyjątku NullReferenceException
+            }
+            catch (ArgumentException exception)
+            {
+                // Obsługa wyjątku typu ArgumentException
+            }
+            catch (InvalidOperationException exception)
+            {
+                bool exceptionHandled = false;
+                // Obsługa wyjątku typu InvalidOperationException
+                // ..
+                if (!exceptionHandled)
+                {
+                    throw;
+                }
+            }
+            catch (SystemException)
+            {
+                // Obsługa wyjątku typu SystemException
+            }
+            catch (Exception exception)
+            {
+                // Obsługa wyjątku typu Exception
+            }
+            finally
+            {
+                // tu umieść kod porządkujący zasoby. Ten kod
+                // jest wykonywany niezależnie od tego, czy wystąpił wyjątek.
+            }
+        }
+    }
+}
---
+++ 11. Obsługa wyjątków/TextNumberParser/TextNumberParser/TextNumberParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace TextNumberParser
+{
+    public sealed class TextNumberParser
+    {
+        public static int Parse(string textDigit)
+        {
+            string[] digitTexts =
+                { "zero", "jeden", "dwa", "trzy", "cztery",
+            "pięć", "sześć", "siedem", "osiem", "dziewięć"};
+
+            int result = Array.IndexOf(
+                digitTexts,
+                // wykorzystanie wprowadzonego w C# 2.0 operatora ??
+                (textDigit ??
+                // wykorzytanie wprowadzonego w C# 7.0 wyrażenia throw
+                throw new ArgumentNullException(nameof(textDigit))
+                ).ToLower());
+            if (result < 0)
+            {
+                // Wykorzystanie wprowadzonego w C# 6.0 operatora nameof
+                throw new ArgumentException(
+                    "Argument nie reprezentuje cyfry",
+                    nameof(textDigit));
+            }
+
+            return result;
+        }
+    }
+}
